2889e859b1ac7ab50347ddf38f20c4d790170615
Sam Watson <samscottwatson@gmail.com>
2021-02-12 21:14:10 +0000

    Created a reverse geocode class

--- app/src/main/java/com/example/geochat_hack/ReverseGeocode.java
+++ app/src/main/java/com/example/geochat_hack/ReverseGeocode.java
@@ -1,16 +1,50 @@
 package com.example.geochat_hack;
 
-class ProvinceLocator{
+import okhttp3.*;
+import java.io.IOException;
+
+class ReverseGeocode{
+
+    //*
     // (51.604798, -1.242574) Lat, Long
+    // Reverse Geocoding
 
-    private int latitude;
-    private int longitude;
+    private double latitude;
+    private double longitude;
+    private String BASE_URL = "https://us1.locationiq.com/v1/reverse.php?";
+    private String access_token = "pk.faeff0542624005061e9477657990282";
+    private final OkHttpClient httpClient = new OkHttpClient();
 
-    public static void main(String[] args){
-
+    public ReverseGeocode(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
-    public String create_bbox(){
-        return this.longitude + "," + this.latitude +"," + this.longitude + "," + this.latitude;
+    public void query(){
+        OkHttpClient obj = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(this.get_url())
+                //.addHeader("custom-key", "mkyong")  // add request headers
+                .addHeader("User-Agent", "GeoChat Bot")
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            // Get response body
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to encode the data into a URL for the API request
+    private String get_url(){
+        String key = "key=" + this.access_token;
+        String format = "&format=json";
+        String latlong = "&lat=" + this.latitude + "&lon=" + this.longitude;
+        String url = this.BASE_URL + key + format + latlong;
+        return url;
     }
 }
--- app/src/main/java/com/example/geochat_hack/Tester.java
+++ app/src/main/java/com/example/geochat_hack/Tester.java
@@ -1,4 +1,9 @@
 package com.example.geochat_hack;
 
 public class Tester {
+
+    public static void main(String[] args){
+        ReverseGeocode rg = new ReverseGeocode(51.604798, -1.242574);
+        rg.query();
+    }
 }
