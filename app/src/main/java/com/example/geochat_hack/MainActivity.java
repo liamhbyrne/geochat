d7a87b7dfa44b51ecbd72c7da1c34a6216e7334b
Liam Byrne <liambyrne0507@gmail.com>
2021-02-12 19:59:25 +0000

    location permission added

--- app/src/main/java/com/example/geochat_hack/MainActivity.java
+++ app/src/main/java/com/example/geochat_hack/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.geochat_hack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.os.Bundle;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1234);
+
     }
 }
