816f94c045f7490a765022e89b90be5036823bd0
Sam Watson <samscottwatson@gmail.com>
2021-02-13 21:05:36 +0000

    Further UI improvments on Login, Main and Settings

--- app/src/main/java/com/example/geochat_hack/SettingsActivity.java
+++ app/src/main/java/com/example/geochat_hack/SettingsActivity.java
@@ -3,9 +3,12 @@ package com.example.geochat_hack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -15,10 +18,13 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SettingsActivity extends AppCompatActivity {
     Switch notificationsSwitch, messageNoisesSwitch;
 
+    Button back_to_main;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        getSupportActionBar().hide();
 
         SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         // Gets the value (false means if it fails to get the value
@@ -51,6 +57,15 @@ public class SettingsActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putBoolean(getString(R.string.message_noise), b);
                 editor.apply();
+            }
+        });
+
+        back_to_main = findViewById(R.id.back_to_main);
+        // Assign button methods
+        back_to_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
             }
         });
     }
